1d3a1c73dd44a0ea5f60cd7c61bdf056f0e9ab80
Michael Douglas <michaelseandouglas@gmail.com>
2017-09-19 12:23:26 -0400

    lots of changes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+'use strict'
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const nunjucks = require('nunjucks')
-
+const routes = require('./routes');
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
+app.use(express.static('public'))
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {console.log(output);});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 
 
 app.use(function (req, res, next) {
@@ -11,38 +17,48 @@ app.use(function (req, res, next) {
         console.log(req.method + ' / ' + 200)        
     }
     console.log(req.path)
-    res.render( 'index', {title: 'Hall of Fame', people: people} );    
     next();
 })
-
-
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views'); // point nunjucks to the proper directory for templates
-
-
+// app.use('/', routes);
+app.get('/',function (req, res, next) {
+    res.render( 'index', {title: 'Hall of Fame', people: people} );    
+})
 app.listen(3000, function () {
     console.log('server listening')
   })
 
-  app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
 
-  app.get('/news', function (req, res) {
-    res.send('No news is good news!')
-  })
 
-  var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
 
+
+
+
+
+
+
+
+
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+
+
+
+
+
+
+
+
+
+//   app.get('/', function (req, res) {
+//     res.send('Hello World!')
+//   })
+
+//   app.get('/news', function (req, res) {
+//     res.send('No news is good news!')
+//   })
