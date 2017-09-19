da3ca9a053d07189da326d92f2afc3dd0c5557ce
Michael Douglas <michaelseandouglas@gmail.com>
2017-09-19 10:05:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+app.use(function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    if(res.statusCode == 200){
+        console.log(req.method + ' / ' + 200)        
+    }
+    console.log(req.path)
+
+    next();
+})
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log('server listening')
+  })
+
+  app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
+
+  app.get('/news', function (req, res) {
+    res.send('No news is good news!')
+  })
