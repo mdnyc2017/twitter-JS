b1b0d32f34aab41cd6393db64bd6fc8dd2341251
Michael Douglas <michaelseandouglas@gmail.com>
2017-09-19 11:19:15 -0400

    finished step 5

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const nunjucks = require('nunjucks')
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+
 
 app.use(function (req, res, next) {
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
     if(res.statusCode == 200){
         console.log(req.method + ' / ' + 200)        
     }
@@ -13,8 +15,14 @@ app.use(function (req, res, next) {
 })
 
 
-console.log('YESSSSSSSSSSSSS!')
-
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+res.render( 'index', {title: 'Hall of Fame', people: people} );
 
 
 app.listen(3000, function () {
@@ -28,3 +36,13 @@ app.listen(3000, function () {
   app.get('/news', function (req, res) {
     res.send('No news is good news!')
   })
+
+  var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
